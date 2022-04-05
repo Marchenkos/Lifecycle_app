75c92cd8fd9aa8e8eb634ab5efab73c82df39ce4
Ksenia <ms.ksi2000@mail.ru>
2022-04-05 11:47:37 +0300

    manage sessions

--- Lifecycle_app/SceneDelegate.swift
+++ Lifecycle_app/SceneDelegate.swift
@@ -1,52 +1,82 @@
-//
-//  SceneDelegate.swift
-//  Lifecycle_app
-//
-//  Created by Kseniya on 21.03.22.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
+    var documentManager: DocumentManager?
+    var currentSession: Session?
 
+    private enum Constants {
+        static let sessionFileName = "sessionInfo"
+        static let sessionFileExtension = "json"
+        static let firstSessionId = 1
+    }
 
+    // Launch
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        documentManager = DocumentManager()
+
+        return
     }
 
+    // Close
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+
     }
 
+    // Unlock
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
+    // Minimize
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        guard let session = currentSession, let sessionStartDate = currentSession?.startDate else {
+            print("Session wasn't started")
+
+            return
+        }
+
+        if let existedDocumentManager = documentManager {
+            session.endSession()
+            let sessionInfo = session.getSessionInfo()
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm"
+            let sessionStart = formatter.string(from: sessionStartDate)
+
+            print("Session finished! Started at \(sessionStart), duration \(session.duration) sec")
+
+            existedDocumentManager.saveData(content: sessionInfo, fileName: Constants.sessionFileName, fileExtension: Constants.sessionFileExtension)
+        }
     }
 
+    // Maximize
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        if let existedDocumentManager = documentManager {
+           let savedSessionInfo = existedDocumentManager.read(fileName: Constants.sessionFileName, fileExtension: Constants.sessionFileExtension)
+
+            guard let savedSession = savedSessionInfo, let lastSession = savedSessionInfo?.last else {
+                createNewSession(sessionId: Constants.firstSessionId)
+
+                return
+            }
+
+            let sessionDurations = savedSession.map({$0.duration })
+            let totalDuration = sessionDurations.reduce(0, +)
+
+            print("Number of sessions - \(savedSession.count), duration -  \(totalDuration) sec")
+
+            createNewSession(sessionId: lastSession.sessionId + 1)
+        }
     }
 
+    // Lock
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
     }
 
+    func createNewSession(sessionId: Int) {
+        let newSession = Session(sessionId: sessionId)
+        newSession.startSession()
 
+        currentSession = newSession
+    }
 }
-
--- Lifecycle_app/ViewController.swift
+++ Lifecycle_app/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  Lifecycle_app
-//
-//  Created by Kseniya on 21.03.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
